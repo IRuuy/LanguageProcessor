41325632644aaa07766978ef9a9e50df6bc5ef47
Daniil Shulgin <manganeko@yandex.ru>
2024-03-02 15:21:56 +0700

    fix: bug with size char

--- lexer/main.cpp
+++ lexer/main.cpp
@@ -6,9 +6,11 @@
 using namespace std;
 
 int main(int argc, char** argv){
+
     string statement;
     cout << "Input statenent: ";
     getline(cin, statement);
+
     try {
         vector<Token> vec = getTokens(statement.data());
         for(Token item : vec)
